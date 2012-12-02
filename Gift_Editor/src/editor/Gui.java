3ab5a9608559bf928a1bf22c01228dc088ac2a68
Fahad Alsharef <albaraky@gmail.com>
2012-12-02 23:49:26 +0000

    MultiChoies design

--- Gift_Editor/src/editor/Gui.java
+++ Gift_Editor/src/editor/Gui.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import net.miginfocom.swing.MigLayout;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
@@ -11,34 +13,58 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JRadioButtonMenuItem;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Gui {
 
 	private JFrame frame;
 	private JTextField textField;
-	private RadioButtonGroupPanel rdbtnmntmNewRadioItem;
+	private JTextField questionTitle_multi;
+	private JTextField choiseTextFilde1;
+	private JTextField choiseTextFilde2;
+	private JTextField choiseTextFilde3;
+	private JTextField choiseTextFilde4;
+	
+	
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Gui window = new Gui();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Gui window = new Gui();
+//					window.frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the application.
 	 */
 	public Gui() {
-		initialize();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					initialize();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
 	}
 
 	/**
@@ -50,8 +76,12 @@ public class Gui {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[][grow]"));
 		
-		JButton btnSave = new JButton("Save");
-		frame.getContentPane().add(btnSave, "flowx,cell 0 0");
+		JButton btnNew = new JButton("New Text File");
+		btnNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnNew, "flowx,cell 0 0");
 		
 		JButton btnClose = new JButton("Close");
 		frame.getContentPane().add(btnClose, "cell 0 0");
@@ -78,16 +108,20 @@ public class Gui {
 		
 		JButton btnSaveToTest = new JButton("Save To test file");
 		panel.add(btnSaveToTest, "cell 0 2,alignx left");
+		ButtonGroup ttt = new ButtonGroup();
 		
-		JPanel panel_6 = new JPanel();
-		panel.add(panel_6, "cell 1 2,grow");
+		JRadioButton rdbtnTrue = new JRadioButton("True");
+		panel.add(rdbtnTrue, "cell 1 2");
 		
-//		JRadioButtonMenuItem rdbtnmntmNewRadioItem = new JRadioButtonMenuItem("New radio item");
-		RadioButtonGroupPanel rdbtnmntmNewRadioItem = new RadioButtonGroupPanel(new String[] {
-				"True", "False" }, "True");
-		panel_6.add(rdbtnmntmNewRadioItem);
+		JRadioButton rdbtnFalse = new JRadioButton("Fales");
+		panel.add(rdbtnFalse, "flowx,cell 1 2");
+		ttt.add(rdbtnTrue);
+		ttt.add(rdbtnFalse);
 		
 		
+		
+		
+	
 		
 		JButton btnCancelclearQuestionText = new JButton("Cancel/Clear Question Text");
 		panel.add(btnCancelclearQuestionText, "cell 4 2,alignx right,aligny top");
@@ -95,8 +129,64 @@ public class Gui {
 		JButton btnAddAnpther = new JButton("Add another True/False Question");
 		panel.add(btnAddAnpther, "cell 0 5 5 1,alignx left");
 		
+		
+		
 		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_1, null);
+		tabbedPane.addTab("Multiiple Choice", null, panel_1, null);
+		panel_1.setLayout(new MigLayout("", "[][grow]", "[][grow][][][][][][][][]"));
+		
+		JLabel lblQuestionTitleoptional_1 = new JLabel("Question Title (optional)");
+		panel_1.add(lblQuestionTitleoptional_1, "cell 0 0,alignx right");
+		
+		questionTitle_multi = new JTextField();
+		panel_1.add(questionTitle_multi, "cell 1 0,growx");
+		questionTitle_multi.setColumns(10);
+		
+		JLabel lblQuestion_1 = new JLabel("Question");
+		panel_1.add(lblQuestion_1, "cell 0 1,alignx right,aligny top");
+		
+		JTextPane question_mulit = new JTextPane();
+		panel_1.add(question_mulit, "cell 1 1,grow");
+		
+		JButton btnCancelclearQuestionText_1 = new JButton("Cancel/Clear Question Text");
+		panel_1.add(btnCancelclearQuestionText_1, "cell 1 2,alignx right");
+		
+		JButton btnAddAnswer = new JButton("Add Answer");
+		panel_1.add(btnAddAnswer, "cell 0 3,alignx left");
+		
+		choiseTextFilde1 = new JTextField();
+		panel_1.add(choiseTextFilde1, "flowx,cell 1 3,growx");
+		choiseTextFilde1.setColumns(10);
+		
+		JSpinner spinner = new JSpinner();
+		panel_1.add(spinner, "cell 1 3");
+		
+		choiseTextFilde2 = new JTextField();
+		panel_1.add(choiseTextFilde2, "flowx,cell 1 4,growx");
+		choiseTextFilde2.setColumns(10);
+		
+		JSpinner spinner_1 = new JSpinner();
+		panel_1.add(spinner_1, "cell 1 4");
+		
+		choiseTextFilde3 = new JTextField();
+		panel_1.add(choiseTextFilde3, "flowx,cell 1 5,growx");
+		choiseTextFilde3.setColumns(10);
+		
+		JSpinner spinner_2 = new JSpinner();
+		panel_1.add(spinner_2, "cell 1 5");
+		
+		choiseTextFilde4 = new JTextField();
+		panel_1.add(choiseTextFilde4, "flowx,cell 1 6,growx");
+		choiseTextFilde4.setColumns(10);
+		
+		JSpinner spinner_3 = new JSpinner();
+		panel_1.add(spinner_3, "cell 1 6");
+		
+		JButton btnSaveToTest_1 = new JButton("Save to test file");
+		panel_1.add(btnSaveToTest_1, "cell 0 7");
+		
+		JButton btnAddAnotherMultiple = new JButton("Add Another Multiple Choice");
+		panel_1.add(btnAddAnotherMultiple, "cell 0 9 2097051 1");
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_2, null);
